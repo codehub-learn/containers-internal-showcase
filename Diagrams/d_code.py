980ad0b98d1d47a13b76456733c279109540d230
littledevel <kostas.makedos@yahoo.com>
2023-09-17 13:48:15 +0300

    Adding latest diagrams

--- Diagrams/d_code.py
+++ Diagrams/d_code.py
@@ -1,9 +1,60 @@
 from diagrams import Diagram,Edge
-from diagrams.aws.compute import EC2
-from diagrams.onprem.client import User
+from diagrams.c4 import Person,Container,SystemBoundary,Database
+
+graph_attr = {
+    "splines": "spline",
+}
 
 
 
-with Diagram("EC2", show=False):
-    User("User") >> Edge(label="commit") >> EC2("machine")
 
+with Diagram("Components in Series", show=False, direction="LR", graph_attr=graph_attr):
+    with SystemBoundary("Components in Series"):
+        lb = Person(
+            name = "Load Balancer",
+            description = "99.999%"
+        )
+        vm = Container(
+            name = "Virtual Machine",
+            description = "99.5%"
+        )
+        app = Container(
+            name = "Application",
+            description = "99.99%"
+        )
+        db = Database(
+            name = "Database",
+            description = "99.9%"
+        )
+        lb >> vm >> app >> db
+    with SystemBoundary("Components in Parallel", color="darkorange"):
+        lb = Person(
+            name = "Load Balancer",
+            description = "99.999%"
+        )
+        vm1 = Container(
+            name = "Virtual Machine",
+            description = "99.5%"
+        )
+        app1 = Container(
+            name = "Application",
+            description = "99.99%"
+        )
+        db1 = Database(
+            name = "Database",
+            description = "99.9%"
+        )
+        vm2 = Container(
+            name = "Virtual Machine",
+            description = "99.5%"
+        )
+        app2 = Container(
+            name = "Application",
+            description = "99.99%"
+        )
+        db2 = Database(
+            name = "Database",
+            description = "99.9%"
+        )
+        lb >> vm1 >> app1 >> db1
+        lb >> vm2 >> app2 >> db2
