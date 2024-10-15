57503bea3e07ca0f1c04f83ff4d1fa8e3c1d0e7f
Kostas Makedos <kostasmakedos@cloud.upwork.com>
2024-10-15 13:32:46 +0300

    Adding hello from Pfizer again

--- java-http/src/main/java/org/codehub/JPrint.java
+++ java-http/src/main/java/org/codehub/JPrint.java
@@ -16,7 +16,7 @@ class JPrint {
     }
 
     private static void handleGet(HttpExchange exchange) throws IOException {
-        String response = "Hello from Pfizer";
+        String response = "Hello from Pfizer again";
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
